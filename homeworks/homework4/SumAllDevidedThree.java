3e5ea5128ba8e69afcc1abb721a39fc1c7b03e98
Student <45074202+coicoin@users.noreply.github.com>
2020-02-02 21:41:19 +0200

    not ready

--- homeworks/homework4/SumAllDevidedThree.java
+++ homeworks/homework4/SumAllDevidedThree.java
@@ -11,21 +11,21 @@ import java.util.Scanner;
 public class SumAllDevidedThree {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] arr = new int[5];
+        
+        //длина массива
+        System.out.println("Введите длину массива: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
 
         //ввод данных в массив
         System.out.println("Заполните массив: ");
-        int num = 0;
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = scanner.nextInt();
-            }
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
 
         //вывод всего массива
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
-
-
-
     }
 }
